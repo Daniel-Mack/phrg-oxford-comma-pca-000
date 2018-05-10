06f12f7d6def2b90d156a268c2c01f8259305353
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-10 16:54:39 -0400

    Built oxford method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   array.join(",")
+  elsif array.length == 2
+   array.insert(1, "and").join(" ")
+  elsif array.length >= 3
+   array[-1] = "and #{array[-1]}"
+   return array.join(", ")
+ end
 end
